a8ea33a90b4e53dca5cc8b9ce9b5e10673fb492b
UI-Citzenship <64647965+UI-Citzenship@users.noreply.github.com>
2021-01-28 13:07:48 +0000

    Update toogle.js

--- js/toogle.js
+++ js/toogle.js
@@ -71,7 +71,9 @@ function myFunction() {
     element.classList.toggle("dark-mode");
 }
 
-btnDark.addEventListener('click', myFunction())
+btn.addEventListener('click', function () {
+    myFunction();
+})
 
 // Snow effect in Canvas
 
